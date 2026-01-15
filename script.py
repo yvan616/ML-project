5ad8e107ff83f4f5b40dfc57bf3cd8b0eb62ed8a
yvan616 <134450924+yvan616@users.noreply.github.com>
2026-01-15 15:10:01 +0200

    another version

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Voiture:
     def __init__(self, marque, modele, annee):
         self.marque = marque
@@ -7,3 +9,11 @@ class Voiture:
         print(f"La voiture {self.marque} {self.modele} démarre.")
     def arreter(self):
         print(f"La voiture {self.marque} {self.modele} s'arrête.")
+
+
+matr = np.arange(12).reshape(3, 4) 
+M = matr[:,-1]
+M = np.zeros((3,))
+
+print(M)
+print(matr)
